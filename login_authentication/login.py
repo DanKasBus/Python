35b2b18a4b9d71359323dea72e9a311a68e414cc
c0de <c0de@c0de.dev>
2023-11-16 16:54:39 -0600

    format with black
    "It can be any color you like... as long as it's black"

--- login_authentication/login.py
+++ login_authentication/login.py
@@ -10,16 +10,14 @@ import time
 import webbrowser
 
 # Define approved users and their corresponding passwords in a dictionary
-APPROVED_USERS: dict = {
-    "dkasp": "qweqweqwe",
-    "jbarr": "qweqweqwe"
-}
+APPROVED_USERS: dict = {"dkasp": "qweqweqwe", "jbarr": "qweqweqwe"}
 
 # Timeout duration in seconds
 TIMEOUT: int = 30
 MAX_ATTEMPTS: int = 4
 
 DEST_LINK: str = "https://www.w3schools.com/python/python_lists.asp"
+
 
 def main():
     """The main method"""
@@ -35,20 +33,23 @@ def main():
         # Check if the input username exists in the dictionary and if the password is correct
         if input_user in APPROVED_USERS and input_pass == APPROVED_USERS[input_user]:
             print("Login successful")
-            
+
             # Open in a web browser- just a random redirect to a tab I had open 101
             webbrowser.open(DEST_LINK)
-            
+
             break  # Exit the loop if login is successful
-        
+
         else:
             print("Incorrect username or password")
             login_attempts += 1
 
             # Check if the maximum attempts have been reached
             if login_attempts >= MAX_ATTEMPTS:
-                print(f"Maximum login attempts reached. Please try again in {TIMEOUT} seconds.")
+                print(
+                    f"Maximum login attempts reached. Please try again in {TIMEOUT} seconds."
+                )
                 time.sleep(TIMEOUT)
+
 
 if __name__ == "__main__":
     main()
