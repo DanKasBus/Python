dbcefd5ae0d42e636cb9e921608ca0b4498d389e
c0de <c0de@c0de.dev>
2023-11-16 16:49:20 -0600

    Fix the loop to run forever
    Previously if MAX_ATTEMPTS was reached, the script would exit after the TIMEOUT was reached

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ import time
 import webbrowser
 
 # Define approved users and their corresponding passwords in a dictionary
-APPROVED_USERS = {
+APPROVED_USERS: dict = {
     "dkasp": "qweqweqwe",
     "jbarr": "qweqweqwe"
 }
@@ -27,10 +27,10 @@ def main():
     # Initialize login attempt counter w/  maximum allowed login attempts
     login_attempts: int = 0
 
-    while login_attempts < max_attempts:
+    while True:
         # Input username and password from the user
-        input_user = input("Enter username: ")
-        input_pass = input("Enter password: ")
+        input_user: str = input("Enter username: ")
+        input_pass: str = input("Enter password: ")
 
         # Check if the input username exists in the dictionary and if the password is correct
         if input_user in APPROVED_USERS and input_pass == APPROVED_USERS[input_user]:
@@ -46,11 +46,9 @@ def main():
             login_attempts += 1
 
             # Check if the maximum attempts have been reached
-            if login_attempts == max_attempts:
+            if login_attempts >= MAX_ATTEMPTS:
                 print(f"Maximum login attempts reached. Please try again in {TIMEOUT} seconds.")
                 time.sleep(TIMEOUT)
-
-
 
 if __name__ == "__main__":
     main()
