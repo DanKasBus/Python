d87ef3ea7afc16a2c5f9e4d414dd391ac7cb9621
c0de <c0de@c0de.dev>
2023-11-16 16:41:43 -0600

    put the actual code into main

--- login.py
+++ login.py
@@ -15,39 +15,41 @@ APPROVED_USERS = {
     "jbarr": "qweqweqwe"
 }
 
-# Initialize login attempt counter w/  maximum allowed login attempts
-login_attempts = 0
-max_attempts = 4
-
-# Timeout duration in seconds
-timeout_duration = 30  # .5 minute
-
-while login_attempts < max_attempts:
-    # Input username and password from the user
-    input_user = input("Enter username: ")
-    input_pass = input("Enter password: ")
-
-    # Check if the input username exists in the dictionary and if the password is correct
-    if input_user in APPROVED_USERS and input_pass == APPROVED_USERS[input_user]:
-        print("Login successful")
-        
-        # Open in a web browser- just a random redirect to a tab I had open 101
-        webbrowser.open("https://www.w3schools.com/python/python_lists.asp")
-        
-        break  # Exit the loop if login is successful
-    
-    else:
-        print("Incorrect username or password")
-        login_attempts += 1
-
-        # Check if the maximum attempts have been reached
-        if login_attempts == max_attempts:
-            print(f"Maximum login attempts reached. Please try again in {timeout_duration} seconds.")
-            time.sleep(timeout_duration)
 
 
 def main():
     """The main method"""
+
+    # Initialize login attempt counter w/  maximum allowed login attempts
+    login_attempts = 0
+    max_attempts = 4
+
+    # Timeout duration in seconds
+    timeout_duration = 30  # .5 minute
+
+    while login_attempts < max_attempts:
+        # Input username and password from the user
+        input_user = input("Enter username: ")
+        input_pass = input("Enter password: ")
+
+        # Check if the input username exists in the dictionary and if the password is correct
+        if input_user in APPROVED_USERS and input_pass == APPROVED_USERS[input_user]:
+            print("Login successful")
+            
+            # Open in a web browser- just a random redirect to a tab I had open 101
+            webbrowser.open("https://www.w3schools.com/python/python_lists.asp")
+            
+            break  # Exit the loop if login is successful
+        
+        else:
+            print("Incorrect username or password")
+            login_attempts += 1
+
+            # Check if the maximum attempts have been reached
+            if login_attempts == max_attempts:
+                print(f"Maximum login attempts reached. Please try again in {timeout_duration} seconds.")
+                time.sleep(timeout_duration)
+
 
 
 if __name__ == "__main__":
